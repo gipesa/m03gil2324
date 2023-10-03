3d143ca6a43d4d92fd933bbfdeef8a5d532ce970
Gillian Pérez Samit <gipe3503@proven.local>
2023-10-03 12:56:09 +0200

    exercici18,parts2-3

--- exercici18_uds2/src/exercici18_uds2/Exercici18_uds2.java
+++ exercici18_uds2/src/exercici18_uds2/Exercici18_uds2.java
@@ -18,6 +18,9 @@ public class Exercici18_uds2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         double t_normal,h_normal,horas,salario_extra,salario,salario_normal,h_extra;
+        final int SALARIO_SIN_IMPUESTOS=500,SALARIO_TOPE_IMPUESTOS = 0;
+        double impuestos=0,salario_neto,salario_impuestos,salario_tope_impuestos;
+        double impuestos_25 = 0;
         System.out.println("Quantes hores has treballat?");
         t_normal = 20;
         h_normal= 35;
@@ -30,12 +33,28 @@ public class Exercici18_uds2 {
             salario_normal = h_normal * t_normal;
             salario = salario_extra + salario_normal;
             System.out.println("El teu salari es " + salario);
-            
         }
         else {
             salario = horas*t_normal;
             System.out.println("El teu salari es " + salario);
         }
+        if(salario>SALARIO_SIN_IMPUESTOS){
+           salario_impuestos = salario - SALARIO_SIN_IMPUESTOS;
+           //System.out.println("Salari a aplicar impuestos " + salario_impuestos);
+           impuestos= (salario_impuestos * 25)/100;
+            System.out.println("impuestos " + impuestos);
+        }
+        else if (salario>SALARIO_TOPE_IMPUESTOS){
+            salario_tope_impuestos = salario - SALARIO_TOPE_IMPUESTOS;
+            impuestos = salario_tope_impuestos * 0.45;
+            salario_impuestos = SALARIO_TOPE_IMPUESTOS - SALARIO_SIN_IMPUESTOS;
+            impuestos_25= impuestos + impuestos_25;
+        }
+        else{
+            impuestos=0;
+        }
+        salario_neto= salario - impuestos;
+        System.out.println("El teu salari es " + salario_neto);
     }
     
 }
